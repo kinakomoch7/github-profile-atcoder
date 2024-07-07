e5bb6eb75686174bef2b93367ab255fc500f3f37
kinakomoch7 <book-insect14@outlook.jp>
2024-07-07 21:27:38 +0900

    [add]ユーザ名取得実装

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,21 @@ import * as core from '@actions/core';
 
 const main = async() : Promise<void> => {
   try {
-    const token = process.env.GITHUB_TOKEN;
 
+    // ユーザのtoken取得
+    const token = process.env.GITHUB_TOKEN;
     if (!token) {
       core.setFailed('GITHUB_TOKEN is not set')
+      return
     }
-    
+
+    // ユーザ名の取得
+    const userName = 3 <= process.argv.length ? process.argv[2] : process.env.USERNAME;
+    if (!userName) {
+      core.setFailed('USERNAME is not set')
+      return
+    }
+
   } catch (error) {
     console.error(error)
     core.setFailed('error')
