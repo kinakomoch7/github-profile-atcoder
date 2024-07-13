ee7b4e63a3a1a80fe6dd2238ea7b2703a4bed0b6
kinakomoch7 <book-insect14@outlook.jp>
2024-07-14 00:10:41 +0900

    [delete]test

--- dist/components/writeFile.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = require("fs");
-const constants_1 = require("../constants/constants");
-const writeFile = (fileName, content) => {
-    (0, fs_1.mkdirSync)(constants_1.OUTPUT_FOLDER, { recursive: true });
-    (0, fs_1.writeFileSync)(`${constants_1.OUTPUT_FOLDER}/${fileName}`, content);
-};
-exports.default = writeFile;
